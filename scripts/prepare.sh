c4c8b40cb880e348c9d93a05778f2f5e5cd3e9b4
Parvez722 <65571098+Parvez722@users.noreply.github.com>
2025-12-18 20:30:21 +0600

    Update prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
-cp ../dts/mt7621_zte_e8820s.dts target/linux/ramips/dts/
+
+echo "==> Copying E8820S DTS"
+cp ../dts/mt7621_zte_e8820s.dts \
+   target/linux/ramips/dts/
+
+echo "==> Applying E8820S patches"
 for p in ../patches/*.patch; do
+  echo "Applying $p"
   patch -p1 < "$p"
 done
+
+echo "==> Prepare done"
